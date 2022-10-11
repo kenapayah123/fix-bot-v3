d7864b58d391710589b8e0da36da0a162e21d2f4
IBNU-MD <109790605+IBNU-MD@users.noreply.github.com>
2022-10-12 05:18:39 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG
+
+Pengen mentahan nya ?
+Nih ada santai bro....
+https://github.com/bisnissaya12
+
+Pengen yang udah di recode oleh owner?
+Script bot ini dijual bila kalian mau bisa chat owner
+
+wa.me/6281807254833`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    title: 'IBNU-MD',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
